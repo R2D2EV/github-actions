8611f194bd5d2011dbd3e2f48775c37e15e99d49
Arturo Espinosa Vargas <r2d2eev@gmail.com>
2025-09-17 22:06:47 -0600

    section7

---
+++ WebPageRepository_2/src/components/HelpBox.jsx
@@ -0,0 +1,13 @@
+import './HelpBox.css';
+
+function HelpBox({ title, text }) {
+  return (
+    <article className="help-box">
+      <h2>{title}</h2>
+      <p>{text}</p>
+      <p>{text}</p>
+    </article>
+  );
+}
+
+export default HelpBox;
